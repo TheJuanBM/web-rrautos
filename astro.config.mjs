f74dd4e3282812432b8621d884577f034672f4dd
Juan Boyaca <jubedoya@mercadolibre.com>
2025-09-26 16:15:41 -0500

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import node from '@astrojs/node'
 import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import cloudflare from '@astrojs/cloudflare'
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
   vite: {
     plugins: [tailwindcss()],
   },
@@ -17,12 +17,7 @@ export default defineConfig({
     format: 'file',
   },
   compressHTML: true,
-  server: {
-    headers: {
-      'X-Content-Type-Options': 'nosniff',
-      'X-Frame-Options': 'DENY',
-      'X-XSS-Protection': '1; mode=block',
-      'Referrer-Policy': 'strict-origin-when-cross-origin',
-    },
-  },
+  adapter: cloudflare({
+    imageService: 'compile',
+  }),
 })
